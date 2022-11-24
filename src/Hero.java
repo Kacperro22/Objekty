6a2913981553d9c15c3f55b8b1295f2ffa96c093
Kacperro22 <118756501+Kacperro22@users.noreply.github.com>
2022-11-24 19:58:38 +0100

    Initial commit

---
+++ src/Hero.java
@@ -0,0 +1,17 @@
+public class Hero {
+    String name;
+    String abilities;
+    int level;
+    double damage;
+    boolean fame;
+
+    Hero(String name, String abilities,int lvel, double damage, boolean fame){
+        this.name = name;
+        this.abilities = abilities;
+        this.level = level;
+        this.damage = damage;
+        this.fame = fame;
+    }
+}
+
+
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Hero Batman = new Hero("Tom", "can fly", 10, 1.5, true);
+
+    }
+}
