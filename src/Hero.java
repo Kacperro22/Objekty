95b3ca00f4de59d28d3df8e743b211567b1e949c
Kacperro22 <118756501+Kacperro22@users.noreply.github.com>
2022-11-24 20:45:57 +0100

    Done

--- src/Hero.java
+++ src/Hero.java
@@ -1,15 +1,55 @@
 public class Hero {
-    String name;
-    String abilities;
-    int level;
-    double damage;
-    boolean fame;
+    private String name;
+    private String abilities;
+    private int level;
+    private double damage;
+    private boolean fame;
 
-    Hero(String name, String abilities,int lvel, double damage, boolean fame){
+    public Hero(String name, String abilities, int level, double damage, boolean fame) {
         this.name = name;
         this.abilities = abilities;
         this.level = level;
         this.damage = damage;
+        this.fame = fame;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAbilities() {
+        return abilities;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public double getDamage() {
+        return damage;
+    }
+
+    public boolean getFame() {
+        return fame;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAbilities(String abilities) {
+        this.abilities = abilities;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public void setDamage(double damage) {
+        this.damage = damage;
+    }
+
+    public void setFame(boolean fame) {
         this.fame = fame;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        Hero Batman = new Hero("Tom", "can fly", 10, 1.5, true);
+        Hero batman = new Hero("Tom", "can fly", 10, 1.5, true);
+        System.out.println(batman.getNick());
+        System.out.println(batman.getAbilities());
+        System.out.println(batman.getLevel());
+        System.out.println(batman.getDamage());
+        System.out.println(batman.getFame());
+
+        System.out.println("__________________");
+
+        batman.setName("John");
+        System.out.println(batman.getNick());
+
+        batman.setAbilities("can run");
+        System.out.println(batman.getAbilities());
+
+        batman.setDamage(5.5);
+        System.out.println(batman.getDamage());
+
+        batman.setFame(false);
+        System.out.println(batman.getFame());
+
 
     }
 }
